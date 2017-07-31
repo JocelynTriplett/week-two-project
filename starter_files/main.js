26eaa9d032baf617a87f9422866994992a547e1e
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-07-31 06:56:24 -0400

    Replaced var with let

--- starter_files/main.js
+++ starter_files/main.js
@@ -9,7 +9,7 @@ console.log(data);
 // 1: Show me how to calculate the average price of all items.
 function question1 () {
   let averagePrice = 0
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     costEach = data[i].price;
     averagePrice += costEach / data.length;
   }
@@ -21,11 +21,11 @@ function question1 () {
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
   let reasonablyPriced = []
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       if (data[i].price >= 14 && data[i].price <= 18)
       reasonablyPriced.push(data[i]);
     }
-    for (var i = 0; i < reasonablyPriced.length; i++) {
+    for (let i = 0; i < reasonablyPriced.length; i++) {
       console.log(reasonablyPriced[i].title + "\n")
     }
 }
@@ -34,7 +34,7 @@ function question2 () {
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     if (data[i].price >= 14 && data[i].price <= 18 && data[i].currency_code == "GBP") {
       console.log(data[i].title + " costs " + data[i].price + " pounds.");
     }
@@ -45,14 +45,14 @@ function question3 () {
 // 4: Display a list of all items who are made of wood.
 function question4 (){
   let itemsMadeOfWood = []
-  for (var i = 0; i < data.length; i++) {
-    for (var m = 0; m < data[i].materials.length; m++) {
+  for (let i = 0; i < data.length; i++) {
+    for (let m = 0; m < data[i].materials.length; m++) {
       if (data[i].materials[m] == "wood") {
         itemsMadeOfWood.push(data[i]);
       }
     }
   }
-  for (var i = 0; i < itemsMadeOfWood.length; i++) {
+  for (let i = 0; i < itemsMadeOfWood.length; i++) {
     console.log(itemsMadeOfWood[i].title + " is made of wood." + "\n")
   }
 }
@@ -62,14 +62,14 @@ function question4 (){
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   let EightOrMoreMaterials = []
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     if (data[i].materials.length >= 8) {
       EightOrMoreMaterials.push(data[i]);
     }
   }
-  for (var i = 0; i < EightOrMoreMaterials.length; i++) {
+  for (let i = 0; i < EightOrMoreMaterials.length; i++) {
     console.log(EightOrMoreMaterials[i].title + " has " + EightOrMoreMaterials[i].materials.length + " materials:" + "\n")
-    for (var m = 0; m < EightOrMoreMaterials[i].materials.length; m++) {
+    for (let m = 0; m < EightOrMoreMaterials[i].materials.length; m++) {
       console.log("- " + EightOrMoreMaterials[i].materials[m] + "\n");
     }
   }
@@ -80,7 +80,7 @@ function question5 () {
 // Answer:
 function question6 () {
   let MadeByMe = []
-  for (var i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     if (data[i].who_made == "i_did") {
       MadeByMe.push(data[i]);
     }
